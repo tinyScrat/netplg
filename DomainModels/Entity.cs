aa04f81df46b04d225294ab1ef135fb5e6850fce
Tech49 <tech49@skytower.cc>
2025-12-07 21:41:24 +0800

    add stuff

---
+++ DomainModels/Entity.cs
@@ -0,0 +1,6 @@
+﻿namespace DomainModels;
+
+public class Entity
+{
+
+}
---
+++ DomainModels/ValueObject.cs
@@ -0,0 +1,27 @@
+namespace DomainModels;
+
+/*
+    Characteristics of value object
+    1. measure, quantify or describe a thing, but not a thing in itself
+        A person is a thing, but name, age are attributes that describe the person
+
+    2. Immunitable
+        A value cannot be changed once created.
+
+    3. Conceptual whole
+        A value that describe money need to have amount and currency, these 2 attributes
+        form a conceptual whole of monetary value
+
+    4. Replaceablity
+        Just like primitive value in a programming language, it got replaced with a new value
+        that represent another value, not parts of it.
+
+    5. Equality
+        The object instance may not be the same, but as long as the attributes and the same,
+        then these 2 value objects are the same.
+*/
+
+public class ValueObject
+{
+
+}
