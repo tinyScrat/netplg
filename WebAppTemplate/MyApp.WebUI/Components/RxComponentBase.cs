2492349a3d86fce4c4ab1c30bcc6cf124763c1cb
Tech49 <tech49@skytower.cc>
2026-02-09 21:26:19 +0800

    add AuthorizeViewExt

--- WebAppTemplate/MyApp.WebUI/Components/RxComponentBase.cs
+++ WebAppTemplate/MyApp.WebUI/Components/RxComponentBase.cs
@@ -2,6 +2,7 @@ namespace MyApp.WebUI.Components;
 
 using System;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using Microsoft.AspNetCore.Components;
 
 public abstract class RxComponentBase : ComponentBase, IDisposable
@@ -9,15 +10,16 @@ public abstract class RxComponentBase : ComponentBase, IDisposable
     private readonly CompositeDisposable _disposables = [];
     private bool _disposed;
 
-    protected void Track(IDisposable disposable)
+    protected IDisposable Track(IDisposable disposable)
     {
         if (_disposed)
         {
             disposable.Dispose();
-            return;
+            return disposable;
         }
 
         _disposables.Add(disposable);
+        return disposable;
     }
 
     protected IDisposable Subscribe<T>(IObservable<T> source, Action<T>? onNext = null)
@@ -32,10 +34,20 @@ public abstract class RxComponentBase : ComponentBase, IDisposable
             });
         });
 
-        Track(sub);
-
-        return sub;
+        return Track(sub);
     }
+
+    protected IDisposable SubscribeAsync<T>(
+        IObservable<T> source,
+        Func<T, Task> handler)
+    {
+        var sub = source
+            .SelectMany(v => Observable.FromAsync(() => handler(v)))
+            .Subscribe(_ => InvokeAsync(StateHasChanged));
+
+        return Track(sub);
+    }
+
 
     public virtual void Dispose()
     {
