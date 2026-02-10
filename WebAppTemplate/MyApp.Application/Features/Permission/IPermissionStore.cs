29777d4374c8c8892b4085ecbe850cdba356f501
Tech49 <tech49@skytower.cc>
2026-02-10 12:26:15 +0800

    remove permission

--- WebAppTemplate/MyApp.Application/Features/Permission/IPermissionStore.cs
+++
@@ -1,8 +0,0 @@
-namespace MyApp.Application.Features.Permission;
-
-public interface IPermissionStore
-{
-    bool IsInitialized { get; }
-    IReadOnlySet<string> Permissions { get; }
-    IObservable<PermissionState> Changes { get; }
-}
